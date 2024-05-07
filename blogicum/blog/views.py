7434e2143cd9a881db13052f5d8d581956a1d451
borodinskiy <anton.borodinskiy@mail.ru>
2024-05-07 23:40:37 +0300

    3st fix

--- blogicum/blog/views.py
+++ blogicum/blog/views.py
@@ -64,7 +64,8 @@ def post_detail(request, post_id):
 
 def category_posts(request, category_slug):
     template = 'blog/category.html'
-    category_posts_list = [post for post in posts if post['category'] == category_slug]
+    category_posts_list = (
+        [post for post in posts if post['category'] == category_slug])
     context = {'posts': category_posts_list,
                'category': category_slug}
     return render(request, template, context)
